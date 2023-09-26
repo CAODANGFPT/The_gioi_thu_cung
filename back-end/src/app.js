9132c7052e32356122483d4f65e7b4468ffeefab
duchoangfe <duchoangfe@gmail.com>
2023-09-27 01:23:15 +0700

    feature/Create-table-Species

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,12 +1,16 @@
 import cors from "cors";
 import express from "express";
+import servicesRouter from "./routers/services";
 import authRouter from "./routers/auth";
 import speciesRouter from "./routers/species";
 
+import petRouter from "./routers/pet";
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api",servicesRouter);
 app.use("/api", authRouter);
 app.use("/api", speciesRouter);
+app.use("/api", petRouter);
 export const viteNodeApp = app;
---
+++ back-end/src/models/services.js
@@ -0,0 +1,67 @@
+import connection from "../db";
+
+export default class Services {
+  static getAllServices() {
+    return new Promise((resolve, reject) => {
+      connection.query("SELECT * FROM services", (err, results) => {
+        if (err) reject(err);
+        resolve(results);
+      });
+    });
+  }
+
+  static getServicesById(id) {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        "SELECT * FROM services WHERE id = ?",
+        [id],
+        (err, results) => {
+          if (err) reject(err);
+          resolve(results[0]);
+        }
+      );
+    });
+  }
+
+  static createServices(name_service, description, price) {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        "INSERT INTO services (name_service, description,price) VALUES (?, ?,?)",
+        [name_service, description, price],
+        (err, results) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results.insertId);
+          }
+        }
+      );
+    });
+  }
+
+  static updateServices(id, name_service, description, price) {
+    const updateSql =
+      "UPDATE services SET name_service = ?, description = ?, price = ? WHERE id = ?";
+    const values = [name_service, description, price, id];
+    return new Promise((resolve, reject) => {
+      connection.query(updateSql, values, (err) => {
+        if (err) reject(err);
+        resolve();
+      });
+    });
+  }
+  
+
+  static deleteServices(id) {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        "DELETE FROM services WHERE id = ?",
+        [id],
+        (err, results) => {
+          if (err) reject(err);
+          resolve(results);
+        }
+      );
+    });
+  }
+}
