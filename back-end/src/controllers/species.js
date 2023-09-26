cea1b498d0ba3e81f69f7d35835c1c59d50df554
duchoangfe <duchoangfe@gmail.com>
2023-09-27 01:03:15 +0700

    feature/Create-table-Species

---
+++ back-end/src/controllers/species.js
@@ -0,0 +1,67 @@
+import Species from "./../models/species";
+import { speciesSchema } from "./../schemas/species";
+
+export const getAll = async (req, res) => {
+  try {
+    const listSetTime = await Species.getListSpecies();
+    res.json(listSetTime);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const getById = async (req, res) => {
+  try {
+    const setTime = await Species.getIdSpecies(req.params.id);
+    if (!setTime) {
+      res.status(404).json({ error: "không tìm thấy" });
+    } else {
+      res.json(setTime);
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const add = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { error } = speciesSchema.validate(req.body);
+    if (error) {
+      const errors = error.details.map((errorItem) => errorItem.message);
+      return res.status(400).json({
+        message: errors,
+      });
+    }
+    const setTimeId = await Species.addSpecies(name);
+    res.json({ id: setTimeId });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { error } = speciesSchema.validate(req.body);
+    if (error) {
+      const errors = error.details.map((errorItem) => errorItem.message);
+      return res.status(400).json({
+        message: errors,
+      });
+    }
+    await Species.updateSpecies(req.params.id, name);
+    res.json({ message: "Sửa thành công" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const remote = async (req, res) => {
+  try {
+    await Species.removeSpecies(req.params.id);
+    res.json({ message: "Xóa thành công" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- back-end/src/db.js
+++ back-end/src/db.js
@@ -1,11 +1,10 @@
 import mysql from "mysql2";
 
 const connection = mysql.createConnection({
-  host: "sql12.freesqldatabase.com",
+  host: "103.252.137.160",
+  user: "hduchoangmobilec_thegioithucung",
   port: 3306,
-  user: "sql12649048",
-  password: "5l17yNeUZp",
-  database: "sql12649048",
+  password: "thegioithucung",
+  database: "hduchoangmobilec_duantotnghiep",
 });
-
 export default connection;
