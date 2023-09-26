ad589ed5e1050e9f42d25a3693d637bdd8e26f6e
LaPhuc <phuclvph20069@fpt.edu.vn>
2023-09-26 23:20:33 +0700

    update table set time data

--- back-end/src/routers/setTime.js
+++ back-end/src/routers/setTime.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { create, list, show, update, remote } from "../controllers/setTime";
+import {checkPermission} from "../middlewares/checkPermission";
 
 const router = Router();
 
 router.get("/listSetTime", list);
 router.get("/setTime/:id", show);
-router.post("/setTime", create);
-router.patch("/setTime/:id", update);
-router.delete("/setTime/:id", remote);
+router.post("/setTime",checkPermission, create);
+router.patch("/setTime/:id",checkPermission, update);
+router.delete("/setTime/:id",checkPermission, remote);
 export default router;
