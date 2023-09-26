d411802f504b10c8610255e2f03c5b7cbe7a9751
Duy <duytv.dev@gmail.com>
2023-09-27 02:05:41 +0700

    create table role

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 import servicesRouter from "./routers/services";
 import authRouter from "./routers/auth";
 import speciesRouter from "./routers/species";
-
+import roleRouter from "./routers/role";
 import petRouter from "./routers/pet";
 const app = express();
 app.use(express.json());
@@ -13,4 +13,5 @@ app.use("/api", servicesRouter);
 app.use("/api", authRouter);
 app.use("/api", speciesRouter);
 app.use("/api", petRouter);
+app.use("/api", roleRouter);
 export const viteNodeApp = app;
