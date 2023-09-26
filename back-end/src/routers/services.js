b20986389bfa39960ac9b8feabf83616ed3dc19a
Duy <duytv.dev@gmail.com>
2023-09-27 00:46:08 +0700

    add checkPermission

--- back-end/src/routers/services.js
+++ back-end/src/routers/services.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { list , showById , create , update , deleteSe} from "../controllers/services";
-
+import { checkPermission } from "../middlewares/checkPermission";
 const router = Router();
 
 router.get("/services", list);
 router.get("/services/:id", showById);
-router.post("/services", create);
-router.put("/services/:id", update);
-router.delete("/services/:id", deleteSe);
+router.post("/services",checkPermission, create);
+router.put("/services/:id",checkPermission, update);
+router.delete("/services/:id",checkPermission, deleteSe);
 
 export default router;
