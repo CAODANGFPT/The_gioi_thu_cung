d7f5983fdcdedc96eaf921d62d5f6eff8d76fa79
LaPhuc <phuclvph20069@fpt.edu.vn>
2023-09-28 00:01:11 +0700

    create table about

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,8 +3,9 @@ import express from "express";
 import servicesRouter from "./routers/services";
 import authRouter from "./routers/auth";
 import speciesRouter from "./routers/species";
-
 import petRouter from "./routers/pet";
+import aboutRouter from "./routers/about";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -13,4 +14,6 @@ app.use("/api", servicesRouter);
 app.use("/api", authRouter);
 app.use("/api", speciesRouter);
 app.use("/api", petRouter);
+app.use("/api", aboutRouter);
+
 export const viteNodeApp = app;
