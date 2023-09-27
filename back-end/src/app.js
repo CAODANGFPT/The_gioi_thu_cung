2bf381979759d92a19a41c7195f1febe7eeeb48b
Duy <duytv.dev@gmail.com>
2023-09-27 21:23:17 +0700

    create table pethouse

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,8 +3,8 @@ import express from "express";
 import servicesRouter from "./routers/services";
 import authRouter from "./routers/auth";
 import speciesRouter from "./routers/species";
-
 import petRouter from "./routers/pet";
+import pethouseRouter from "./routers/pethouse";
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -13,4 +13,6 @@ app.use("/api", servicesRouter);
 app.use("/api", authRouter);
 app.use("/api", speciesRouter);
 app.use("/api", petRouter);
+app.use("/api", pethouseRouter)
+
 export const viteNodeApp = app;
---
+++ back-end/src/controllers/pethouse.js
@@ -0,0 +1,77 @@
+import Pethouse from "../models/pethouse";
+import { pethouseSchema } from "../schemas/pethouse";
+
+export const list = async (req, res) => {
+  try {
+    const pethouse = await Pethouse.getAllPethouse();
+    res.json(pethouse);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const showById = async (req, res) => {
+  try {
+    const pethouse = await Pethouse.getPethouseById(req.params.id);
+    if (!pethouse) {
+      res.status(404).json({ error: "Pethouse không tồn tại" });
+    } else {
+      res.json(pethouse);
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const { name, status_id } = req.body;
+    const { error } = pethouseSchema.validate(req.body);
+    if (error) {
+      const errors = error.details.map((errorItem) => errorItem.message);
+      return res.status(400).json({
+        message: errors,
+      });
+    }
+    const pethouseId = await Pethouse.createPethouse(name, status_id);
+    res.json({ id: pethouseId });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const { name, status_id } = req.body;
+    const { error } = pethouseSchema.validate(req.body);
+    if (error) {
+      const errors = error.details.map((errorItem) => errorItem.message);
+      return res.status(400).json({
+        message: errors,
+      });
+    }
+    const existingPethouse = await Pethouse.getPethouseById(req.params.id);
+    if (!existingPethouse) {
+      return res.status(404).json({ error: "Pethouse không tồn tại" });
+    }
+    await Pethouse.updatePethouse(req.params.id, name, status_id);
+    res.json({ message: "Cập nhập thông tin pethouse thành công" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const deletePe = async (req, res) => {
+  try {
+    const pethouse = await Pethouse.getPethouseById(req.params.id);
+
+    if (!pethouse) {
+      return res.status(404).json({ message: "Không có gì để xóa" });
+    }
+
+    await Pethouse.deletePethouse(req.params.id);
+    res.json({ message: "pethouse xóa thành công" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
