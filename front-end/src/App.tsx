bbeb86064aa7e9c3826b6573b14269e6c47de54f
Phuclvph20069 <phuclvph20069@fpt.edu.vn>
2023-10-18 22:10:40 +0700

    create base

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./page/base/home";
 
 function App() {
-  return <div>Thế giới thú cưng</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ front-end/src/layoutBase/footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const FooterBase = () => {
+  return (
+    <div>
+      <p>footer</p>
+    </div>
+  );
+};
+export default FooterBase;
