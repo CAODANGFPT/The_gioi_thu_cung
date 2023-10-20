598a9235d5ecf8d6f982f688e8b6cc5c86e467c2
duchoangfe <duchoangfe@gmail.com>
2023-10-20 13:56:30 +0700

    create reset password page UI

--- front-end/src/page/base/account/reset-password/index.tsx
+++ front-end/src/page/base/account/reset-password/index.tsx
@@ -1,21 +1,49 @@
+import * as yup from "yup";
+import { useFormik } from "formik";
 import "../../../../assets/scss/layout/resetPassword.scss";
-import GoogleIcon from "../../../../assets/svg/googleIcon";
 import FooterBase from "../../../../layoutBase/footer";
 import HeaderBase from "./../../../../layoutBase/header";
+import { Link } from "react-router-dom";
+import GoogleIcon from "../../../../assets/svg/googleIcon";
+import AppleIcon from "../../../../assets/svg/appleIcon";
 import FacebookIcon from "./../../../../assets/svg/facebookIcon";
 
 const ResetPassWord = () => {
+  const ResetPasswordSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Email không hợp lệ")
+      .required("Vui lòng điền email"),
+  });
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema: ResetPasswordSchema,
+    onSubmit: (values) => {},
+  });
   return (
     <div>
       <HeaderBase />
       <div className="resetPassword-form">
-        <form className="">
+        <form onSubmit={formik.handleSubmit}>
           <br />
           <h2>Reset Password</h2>
           <br />
           <div className="form-group">
-            <input required className="input" type="text" placeholder="Email" />
+            <input
+              className="input"
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
           </div>
+          {formik.touched.email && formik.errors.email ? (
+            <div className="error-message">{formik.errors.email}</div>
+          ) : null}
 
           <button className="btn-resetpass" type="submit">
             NEXT
@@ -27,16 +55,26 @@ const ResetPassWord = () => {
           <div className="line"></div>
         </div>
         <br />
-        <div className="form-group">
-          <button className="button ">
-            <FacebookIcon />
-            Continue with Facebook
-          </button>
-        </div>
-        <div className="form-group">
-          <button className="button ">
+        <div className="btn-flex">
+          <button className="button">
             <GoogleIcon />
-            Continue with Google
+            <Link to="" className=" custom-link">
+              Đăng nhập bằng google
+            </Link>
+          </button>
+
+          <button className="button">
+            <FacebookIcon />
+            <Link to="" className=" custom-link">
+              Đăng nhập bằng facebook
+            </Link>
+          </button>
+
+          <button className="button">
+            <AppleIcon />
+            <Link to="" className=" custom-link">
+              Đăng nhập bằng Apple
+            </Link>
           </button>
         </div>
       </div>
