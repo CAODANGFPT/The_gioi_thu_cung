96544709cb3841b45a9bde53649c72632f4d6988
Thuc241203 <thucngo241203@gmail.com>
2023-10-20 11:13:06 +0700

    fix validate and text

--- front-end/src/page/base/signup/index.tsx
+++ front-end/src/page/base/signup/index.tsx
@@ -1,22 +1,42 @@
-import "../../../../assets/scss/layout/signUp.scss";
-import logo from "../../../../assets/image/logo.png";
-import banner from "../../../../assets/image/banner.png";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import "../../../assets/scss/layout/signUp.scss";
+import logo from "../../../assets/image/logo.png";
+import banner from "../../../assets/image/banner.png";
 import GoogleIcon from "../../../assets/svg/googleIcon";
 import FacebookIcon from "../../../assets/svg/facebookIcon";
 import { Link } from "react-router-dom";
 import AppleIcon from "../../../assets/svg/appleIcon";
+
 const Signup = () => {
+  const validationSchema = yup.object().shape({
+    username: yup
+      .string()
+      .min(6, "tên phải có ít nhất 6 ký tự")
+      .required("Vui lòng nhập tên tài khoản"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log("Submitted form:", values);
+    },
+  });
+
   return (
     <div className="container">
       <div className="singup-top">
         <img src={logo} alt="logo" />
         <Link to="" className="help">
-          Chợ giúp?
+          Trợ giúp?
         </Link>
       </div>
       <div className="singup-bottom">
         <img className="img-bg" src={banner} alt="" />
-        <form className="f-singup">
+        <form className="f-singup" onSubmit={formik.handleSubmit}>
           <h1>Đăng ký</h1>
           <br />
           <p>
@@ -27,10 +47,18 @@ const Signup = () => {
           </p>
           <div className="input-flex">
             <input
-              className="btn-f "
+              className="btn-f"
               type="text"
               placeholder="Số điện thoại/Tên tài khoản/Email"
+              id="username"
+              name="username"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.username && formik.errors.username && (
+              <div className="error">{formik.errors.username}</div>
+            )}
           </div>
           <button className="btn-f bg-submit" type="submit">
             tiếp theo
@@ -73,4 +101,5 @@ const Signup = () => {
     </div>
   );
 };
+
 export default Signup;
