f9381545336020fbc601b498603c6bcff4f04259
Thanh03Dev <trongthanh2003.tn@gmail.com>
2023-10-22 20:33:06 +0700

    feat:Create-Appointment-UI

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./page/base/home";
+import Appointments from "./page/base/appointments";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Home />}>
           <Route index element={<Home />} />
         </Route>
+        <Route path="appointment" element={<Appointments />} />
       </Routes>
     </BrowserRouter>
   );
