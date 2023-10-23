cf24310f57cfc75f36b3ea537406980a2ae20a8e
duchoangfe <duchoangfe@gmail.com>
2023-10-24 03:47:14 +0700

    fix reset password page

--- front-end/src/page/base/account/reset-password/index.tsx
+++ front-end/src/page/base/account/reset-password/index.tsx
@@ -1,85 +1,89 @@
-import * as yup from "yup";
 import { useFormik } from "formik";
-import "../../../../assets/scss/layout/resetPassword.scss";
-import FooterBase from "../../../../layoutBase/footer";
-import HeaderBase from "./../../../../layoutBase/header";
-import { Link } from "react-router-dom";
-import GoogleIcon from "../../../../assets/svg/googleIcon";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../../../assets/image/logo.png";
+import "../../../../assets/scss/page/resetPassword.scss";
 import AppleIcon from "../../../../assets/svg/appleIcon";
-import FacebookIcon from "./../../../../assets/svg/facebookIcon";
+import FacebookIcon from "../../../../assets/svg/facebookIcon";
+import GoogleIcon from "../../../../assets/svg/googleIcon";
+import {
+  ResetPassRequestSchema,
+  TResetPass,
+} from "../../../../schema/resetPassword";
 
 const ResetPassWord = () => {
-  const ResetPasswordSchema = yup.object().shape({
-    email: yup
-      .string()
-      .email("Email không hợp lệ")
-      .required("Vui lòng điền email"),
-  });
-  const formik = useFormik({
+  const navigate = useNavigate();
+  const formik = useFormik<TResetPass>({
     initialValues: {
       email: "",
     },
-    validationSchema: ResetPasswordSchema,
+    validationSchema: ResetPassRequestSchema,
     onSubmit: (values) => {},
   });
+
   return (
-    <div>
-      <HeaderBase />
-      <div className="resetPassword-form">
-        <form onSubmit={formik.handleSubmit}>
+    <div className="resetPassword">
+      <div className="resetPassword-top">
+        <img src={logo} alt="logo" />
+        <Link to="" className="help">
+          Trợ giúp?
+        </Link>
+      </div>
+      <div className="resetPassword-bottom">
+        <div className="f-resetPassword">
+          <form className="" onSubmit={formik.handleSubmit}>
+            <h1>Quên Mật Khẩu</h1>
+            <br />
+
+            <div className="input-flex">
+              <input
+                className="btn-f"
+                type="text"
+                placeholder="Email"
+                id="email"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              {formik.touched.email && formik.errors.email && (
+                <div className="error">{formik.errors.email}</div>
+              )}
+            </div>
+            <button className="btn-f bg-submit" type="submit">
+              tiếp theo
+            </button>
+          </form>
           <br />
-          <h2>Reset Password</h2>
-          <br />
-          <div className="form-group">
-            <input
-              className="input"
-              type="text"
-              name="email"
-              placeholder="Email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
+          <div className="or">
+            <div className="or-border" />
+            <div className="or-title">Hoặc</div>
+            <div className="or-border" />
           </div>
-          {formik.touched.email && formik.errors.email ? (
-            <div className="error-message">{formik.errors.email}</div>
-          ) : null}
 
-          <button className="btn-resetpass" type="submit">
-            NEXT
-          </button>
-        </form>
-        <div className="line-all">
-          <div className="line"></div>
-          <span className="or">or</span>
-          <div className="line"></div>
-        </div>
-        <br />
-        <div className="btn-flex">
-          <button className="button">
-            <GoogleIcon />
-            <Link to="" className=" custom-link">
-              Đăng nhập bằng google
-            </Link>
-          </button>
-
-          <button className="button">
-            <FacebookIcon />
-            <Link to="" className=" custom-link">
-              Đăng nhập bằng facebook
-            </Link>
-          </button>
-
-          <button className="button">
-            <AppleIcon />
-            <Link to="" className=" custom-link">
-              Đăng nhập bằng Apple
-            </Link>
-          </button>
+          <div className="btn-flex">
+            <button className="btn-f bg-with">
+              <GoogleIcon />
+              <Link to="" className="social">
+                Đăng nhập bằng google
+              </Link>
+            </button>
+            <button className="btn-f bg-with">
+              <FacebookIcon />
+              <Link to="" className="social">
+                Đăng nhập bằng facebook
+              </Link>
+            </button>
+            <button className="btn-f bg-with">
+              <AppleIcon />
+              <Link to="" className="social">
+                Đăng nhập bằng Apple
+              </Link>
+            </button>
+          </div>
         </div>
       </div>
-      <FooterBase />
     </div>
   );
 };
+
 export default ResetPassWord;
