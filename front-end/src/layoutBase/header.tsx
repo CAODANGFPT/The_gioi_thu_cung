d662dd94f69325b9ebf47ced91a968a397f5ea20
LaPhuc <phuclvph20069@fpt.edu.vn>
2023-10-26 21:03:32 +0700

    intergate api

---
+++ front-end/src/layoutBase/header.tsx
@@ -0,0 +1,10 @@
+import '../assets/scss/layout/layoutBase.scss'
+
+const HeaderBase = () => {
+  return (
+    <div>
+      <p className="header">header</p>
+    </div>
+  );
+};
+export default HeaderBase;
---
+++ front-end/src/page/base/home/index.tsx
@@ -0,0 +1,14 @@
+import FooterBase from "../../../layoutBase/footer";
+import HeaderBase from "../../../layoutBase/header";
+
+
+const Home = () => {
+  return (
+    <div>
+      <HeaderBase />
+      <p>home</p>
+      <FooterBase />
+    </div>
+  );
+};
+export default Home;
