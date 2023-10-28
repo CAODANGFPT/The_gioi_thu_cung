e0c7be0557c5d43fd7f078f021ab0d4e394de8cd
LaPhuc <phuclvph20069@fpt.edu.vn>
2023-10-28 23:48:29 +0700

    get api review admin

--- back-end/src/models/reviews.js
+++ back-end/src/models/reviews.js
@@ -3,7 +3,7 @@ import connection from "../db";
 export default class Reviews {
   static getListReviews() {
     return new Promise((resolve, reject) => {
-      connection.query("SELECT reviews.id, reviews.rating, reviews.comment, reviews.created_at, reviews.services_id, users.name as user_name FROM reviews JOIN  users on reviews.user_id = users.id", (err, results) => {
+      connection.query("SELECT reviews.id, reviews.rating, reviews.comment, reviews.created_at, reviews.services_id, reviews.is_delete, users.name as user_name FROM reviews JOIN  users on reviews.user_id = users.id", (err, results) => {
         if (err) reject(err);
         resolve(results);
       });
@@ -41,6 +41,20 @@ export default class Reviews {
       connection.query(
         "DELETE FROM reviews WHERE id = ?",
         [id],
+        (err, results) => {
+          if (err) reject(err);
+          resolve(results);
+        }
+      );
+    });
+  }
+
+
+  static updateBlockReview(id, is_delete) {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        "UPDATE reviews SET is_delete = ? WHERE id = ?",
+        [is_delete, id],
         (err, results) => {
           if (err) reject(err);
           resolve(results);
