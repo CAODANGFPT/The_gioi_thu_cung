57c6e3eac3f8f0f41ce8ed4288a5e0a74e5c008c
duchoangfe <duchoangfe@gmail.com>
2023-10-30 04:43:45 +0700

    get api delete news admin

--- front-end/src/services/news.ts
+++ front-end/src/services/news.ts
@@ -6,6 +6,16 @@ const newsApi = createApi({
   tagTypes: ["News"],
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:8080/api",
+      prepareHeaders: (headers) => {
+      const user = localStorage.getItem("user");
+      if (user) {
+        const { accessToken } = JSON.parse(user);
+        if (accessToken) {
+          headers.set("Authorization", "Bearer " + accessToken);
+        }
+      }
+      return headers;
+    },
   }),
   endpoints(builder) {
     return {
@@ -16,11 +26,21 @@ const newsApi = createApi({
             method: "GET",
           };
         },
+            providesTags: ["News"],
+      }),
+       removeNews: builder.mutation<TNews, number>({
+        query: (id) => {
+          return {
+            url: `/news/${id}`,
+            method: "DELETE",
+          };
+        },
+        invalidatesTags: ["News"],
       }),
     };
   },
 });
 
-export const { useNewsQuery } = newsApi;
+export const { useNewsQuery, useRemoveNewsMutation } = newsApi;
 export const newsReducer = newsApi.reducer;
 export default newsApi;
