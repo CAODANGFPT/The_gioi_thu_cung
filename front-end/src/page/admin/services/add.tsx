5f3ab6e30c47a29a2ed4145e221f8d62c4fb95e0
hai20112003 <nguyenhai20112003@gmail.com>
2023-10-30 22:53:38 +0700

    [FE] Fix upload cloudinary

--- front-end/src/page/admin/services/add.tsx
+++ front-end/src/page/admin/services/add.tsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input, InputNumber, message } from "antd";
+import { Button, Form, Input, InputNumber, Upload, message } from "antd";
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
@@ -6,29 +6,37 @@ import { useAddServicesMutation } from "../../../services/services";
 import { TServicesRequest } from "../../../schema/services";
 import { useNavigate } from "react-router-dom";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
-
+import "../../../assets/scss/page/servicesAdmin.scss"
 const AddService = () => {
-  // const [image, setImage] = useState<string | null>(null);
+  const [image, setImage] = useState<any | null>(null);
   const [value, setValue] = useState("");
   const [addServices, { reset, isLoading: isAddLoading }] =
     useAddServicesMutation();
   const navigate = useNavigate();
-
+  const handleImageChange = (info: any) => {
+    if (info.file.status === "done") {
+      message.success(`${info.file.name} file uploaded successfully`);
+      setImage(info.file.response.url);
+    } else if (info.file.status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  };
   const onFinish = async (values: TServicesRequest) => {
-    const { name, price, image, description } = values;
+    const { name, price, description } = values;
     const servicesData = {
       name,
       price,
-      image,
+      image: image,
       description,
     };
     try {
+      console.log(servicesData);
+      
       await addServices(servicesData).unwrap();
       message.success("Product added successfully");
       reset();
       navigate("/admin/services");
       console.log(servicesData);
-      
     } catch (error) {
       message.error("Failed to add product");
     }
@@ -36,15 +44,6 @@ const AddService = () => {
   const onFinishFailed = async (values: any) => {
     console.log("Failed:", values);
   };
-  // const handleImageChange = (info: any) => {
-  //   if (info.file.status === "done") {
-  //     message.success(`${info.file.name} file uploaded successfully`);
-  //     setImage(info.file.response.url);
-  //   } else if (info.file.status === "error") {
-  //     message.error(`${info.file.name} file upload failed.`);
-  //   }
-  // };
-  // if (isAddLoading) return <Skeleton />;
   return (
     <>
       <h1 className="mt-5 text-3xl font-semibold text-center text-black md:ml-16 md:text-left dark:text-white">
@@ -61,33 +60,26 @@ const AddService = () => {
         >
           <Form.Item
             label={
-              <span className="text-base dark:text-white">Tên dịch vụ</span>
+              <span className="">Tên dịch vụ</span>  
             }
             name="name"
             rules={[{ required: true, message: "Vui lòng nhập tên dịch vụ!" }]}
           >
-            <Input className="dark:hover:border-[#00c6ab] transition-colors duration-300" />
+            <Input className="dark:hover:border-[#00c6ab] transition-colors duration-300 inputForm" />
           </Form.Item>
           <Form.Item
-            label={<span className="text-base dark:text-white">Image</span>}
-            name="image"
-            rules={[{ required: true, message: "Vui lòng nhập image!" }]}
-          >
-            <Input className="dark:hover:border-[#00c6ab] transition-colors duration-300" />
-          </Form.Item>
-          {/* <Form.Item
             label={
-              <span className="text-base dark:text-white">Ảnh sản phẩm</span>
+              <span className="">Ảnh danh mục</span>
             }
             name="picture-card"
             rules={[{ required: true, message: "Vui lòng chọn ảnh" }]}
           >
             <Upload
               name="file"
-              action="https://api.cloudinary.com/v1_1/dqqfnp0hk/image/upload"
+              action="https://api.cloudinary.com/v1_1/dksgvucji/image/upload"
               data={{
-                upload_preset: "asm-web209",
-                cloud_name: "dqqfnp0hk",
+                upload_preset: "wh3rdke8",
+                cloud_name: "dksgvucji",
               }}
               showUploadList={false}
               className="ant-upload-wrapper ant-upload-select"
@@ -95,14 +87,14 @@ const AddService = () => {
             >
               <label
                 htmlFor="images"
-                className="drop-container w-full"
+                className="w-full drop-container"
                 id="dropcontainer"
               >
                 <div
                   className={
                     !image
-                      ? "flex justify-center flex-col items-center"
-                      : "hidden"
+                      ? "imgeBlog"
+                      : "imgeHidden"
                   }
                 >
                   <span className="drop-title">Thả tập tin ở đây</span>
@@ -119,7 +111,7 @@ const AddService = () => {
                 )}
               </label>
             </Upload>
-          </Form.Item> */}
+          </Form.Item>
           <Form.Item
             label={
               <span className="text-base dark:text-white">Giá dịch vụ </span>
@@ -132,24 +124,32 @@ const AddService = () => {
               className="dark:hover:border-[#00c6ab] w-full transition-colors duration-300"
             />
           </Form.Item>
-          <Form.Item  label={
-              <span className="text-base dark:text-white">Mô tả</span>
-            } name="description"
-            rules={[{ required: true, message: "Vui lòng nhập giá mô tả dịch vụ!" }]}>
-            <ReactQuill style={{height:500}} theme="snow" value={value} onChange={setValue} />
+          <Form.Item
+            label={<span className="text-base dark:text-white">Mô tả</span>}
+            name="description"
+            rules={[
+              { required: true, message: "Vui lòng nhập giá mô tả dịch vụ!" },
+            ]}
+          >
+            <ReactQuill
+              style={{ height: 500 }}
+              theme="snow"
+              value={value}
+              onChange={setValue}
+            />
           </Form.Item>
-          <Form.Item >
+          <Form.Item>
             <Button
-              style={{marginTop: 30}}
+              style={{ marginTop: 30 }}
               htmlType="submit"
               className="text-black transition-colors duration-300 dark:text-white"
               size="large"
             >
               {isAddLoading ? (
-                          <AiOutlineLoading3Quarters className="animate-spin" />
-                      ) : (
-                          "Thêm dịch vụ"
-                      )}
+                <AiOutlineLoading3Quarters className="animate-spin" />
+              ) : (
+                "Thêm dịch vụ"
+              )}
             </Button>
           </Form.Item>
         </Form>
