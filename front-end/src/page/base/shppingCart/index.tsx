c782e71370ac888bf881a37b2beef913a7e1e604
hai20112003 <nguyenhai20112003@gmail.com>
2023-11-15 22:31:48 +0700

    ui list shopping

--- front-end/src/page/base/shppingCart/index.tsx
+++ front-end/src/page/base/shppingCart/index.tsx
@@ -1,6 +1,6 @@
 import "../../../assets/scss/page/shoppingCart.scss";
 import { Breadcrumbs } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import imge from "../../../assets/image/project12.png";
 import TrashAlt from "../../../assets/svg/trash-alt";
@@ -8,10 +8,46 @@ import TrashAlt from "../../../assets/svg/trash-alt";
 type Props = {};
 
 const ShoppingCart = (props: Props) => {
+
+  const [dataOrder, setdataOrder] = useState<any>([]);
+  const [quantity, setQuantity] = useState<number>(1);
   function handleClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
     event.preventDefault();
     console.info("You clicked a breadcrumb.");
   }
+  const handleDecreaseQuantity = (id: number) => {
+    setQuantity(id-1)
+    // const cart = dataOrder.map((cartItem: { id: number; quantity: number; quantityProduct: number; }) =>
+    //   cartItem.id === id && cartItem.quantity < cartItem.quantityProduct
+    //     ? { ...cartItem, quantity: cartItem.quantity + 1 }
+    //     : cartItem
+    // );
+    // setdataOrder(cart);
+    // updateOrderMutation({
+    //     id: id,
+    //     quantity: cart.find((item: { id: number }) => item.id === id)?.quantity
+    // });
+  };
+  const handleIncreaseQuantity = (id: number) => {
+    setQuantity(id+1)
+    // const cart = dataOrder.map((cartItem: { id: number; quantity: number; }) =>
+    //   cartItem.id === id && cartItem.quantity != 0
+    //     ? { ...cartItem, quantity: cartItem.quantity - 1 }
+    //     : cartItem
+    // );
+    // const check = cart.find((item: { id: number }) => item.id === id)?.quantity;
+    // if(check > 0){
+    //   updateOrderMutation({
+    //     id: id,
+    //     quantity: cart.find((item: { id: number }) => item.id === id)?.quantity
+    //   });
+    //   setdataOrder(cart);
+    // }else{
+    //   const confirm = window.confirm("Bạn có muốn xóa");
+    //   if(confirm){
+    //     removeOrderMutation(id);
+    //   }
+    }
   return (
     <div className="shoppingCart">
       <div className="breadcrumbs" role="presentation" onClick={handleClick}>
@@ -38,7 +74,7 @@ const ShoppingCart = (props: Props) => {
               </tr>
             </thead>
           </table>
-          <table className="shoppingCart-blog-left-table">
+          <table border={0} className="shoppingCart-blog-left-table">
             <tbody className="shoppingCart-blog-left-table-tbody">
               <tr>
                 <td className="checkbox">
@@ -61,9 +97,23 @@ const ShoppingCart = (props: Props) => {
                 </td>
                 <td className="quantity">
                   <div className="quantity-blog">
-                    <button>-</button>
-                    <div>1</div>
-                    <button>+</button>
+                    <button
+                      className="border border-slate-200 h-6 w-6 lg:h-8 lg:w-8"
+                      onClick={() => handleIncreaseQuantity(quantity)}
+                    >
+                      -
+                    </button>
+                    <input
+                      type="text"
+                      className="border border-slate-200 h-6 w-6 lg:h-8 lg:w-8 text-center"
+                      value={quantity}
+                    />
+                    <button
+                      className="border border-slate-200 h-6 w-6 lg:h-8 lg:w-8"
+                      onClick={() => handleDecreaseQuantity(quantity)}
+                    >
+                      +
+                    </button>
                   </div>
                 </td>
                 <td className="sum">1232131</td>
