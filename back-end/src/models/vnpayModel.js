daffeab10e1e48f8dc7f059eb9e4bed854eb490e
duchoangfe <duchoangfe@gmail.com>
2023-11-23 07:27:15 +0700

    create payment online VNPAY

---
+++ back-end/src/models/vnpayModel.js
@@ -0,0 +1,62 @@
+import { format } from "date-fns";
+import qs from "qs";
+import crypto from "crypto";
+import { Buffer } from "buffer";
+
+export const createPaymentUrl = (req) => {
+  let date = new Date();
+  let createDate = format(date, "yyyyMMddHHmmss");
+
+  let ipAddr =
+    req.headers["x-forwarded-for"] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+
+  let tmnCode = "6DZB8UJI";
+  let secretKey = "WGKMVOVSQGJHRFGIGXNGXMEANKJBMRTH";
+  let vnpUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+  let returnUrl = "http://localhost:8080/order/duchoang";
+  let orderId = format(date, "ddHHmmss");
+  let amount = req.body.amount;
+
+  let currCode = "VND";
+  let vnp_Params = {};
+  vnp_Params["vnp_Version"] = "2.1.0";
+  vnp_Params["vnp_Command"] = "pay";
+  vnp_Params["vnp_TmnCode"] = tmnCode;
+  vnp_Params["vnp_Locale"] = "vn";
+  vnp_Params["vnp_CurrCode"] = currCode;
+  vnp_Params["vnp_TxnRef"] = orderId;
+  vnp_Params["vnp_OrderInfo"] = "Thanh toan cho ma GD:" + orderId;
+  vnp_Params["vnp_OrderType"] = "other";
+  vnp_Params["vnp_Amount"] = amount * 100;
+  vnp_Params["vnp_ReturnUrl"] = returnUrl;
+  vnp_Params["vnp_IpAddr"] = ipAddr;
+  vnp_Params["vnp_CreateDate"] = createDate;
+  vnp_Params = sortObject(vnp_Params);
+
+  let signData = qs.stringify(vnp_Params, { encode: false });
+  let hmac = crypto.createHmac("sha512", secretKey);
+  let signed = hmac.update(Buffer.from(signData, "utf-8")).digest("hex");
+  vnp_Params["vnp_SecureHash"] = signed;
+  vnpUrl += "?" + qs.stringify(vnp_Params, { encode: false });
+
+  return vnpUrl;
+};
+
+function sortObject(obj) {
+  let sorted = {};
+  let str = [];
+  let key;
+  for (key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      str.push(encodeURIComponent(key));
+    }
+  }
+  str.sort();
+  for (key = 0; key < str.length; key++) {
+    sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+  }
+  return sorted;
+}
