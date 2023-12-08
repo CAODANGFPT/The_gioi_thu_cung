9f89ddcc725f351585a8ef0b643ba2deb7ca982d
Thuc241203 <thucngo241203@gmail.com>
2023-12-08 12:35:19 +0700

    fix print invoice Cash

--- back-end/src/routers/appointments.js
+++ back-end/src/routers/appointments.js
@@ -12,6 +12,7 @@ import {
   listAppointmentData,
   searchAppointmentsAdmin,
   show,
+  status_payment,
   update,
   updateAdmin,
   updateAppointmentStatus,
@@ -23,17 +24,16 @@ const router = Router();
 
 router.get("/appointments", list);
 router.get("/appointment/:id", show);
+router.get("/appointment/:id/status_payment", status_payment);
 router.get("/getAllAppointmentData", listAppointmentData);
 router.get("/getAppointmentUser", getAppointmentUser);
 router.get("/getAppointmentUserStatus/:status_id", getAppointmentUserStatus);
 router.post("/searchAppointmentsAdmin", searchAppointmentsAdmin);
 
-
-
 router.patch("/updateAdmin/:id", checkPermission, updateAdmin);
 router.post("/appointment", create);
 
-router.post("/appointmentAdmin",checkPermission, createAdmin);
+router.post("/appointmentAdmin", checkPermission, createAdmin);
 
 router.patch("/appointment/:id", update);
 router.put("/appointmentStatus/:id", updateAppointmentStatus);
