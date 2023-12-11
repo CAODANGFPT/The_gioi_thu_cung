1d53a0f2c2d9a5f5908b5acc0109716e1d2fa473
Duy <duytv.dev@gmail.com>
2023-12-12 03:19:39 +0700

    fix show breed and search somethings

--- front-end/src/page/admin/breed/list.tsx
+++ front-end/src/page/admin/breed/list.tsx
@@ -1,14 +1,29 @@
 import { Button, Popconfirm, message } from "antd";
 import type { ColumnsType } from "antd/es/table";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PlusOutlined } from "@ant-design/icons";
 import { Link, useNavigate } from "react-router-dom";
 import TableAdmin from "../../../components/table";
 import { TBreed } from "../../../schema/breed";
-import { useBreedQuery, useRemoveBreedMutation } from "../../../services/breed";
+import { useBreedQuery, useGetAllBreedQuery, useRemoveBreedMutation } from "../../../services/breed";
+import Search from "antd/es/input/Search";
 const BreedAdmin: React.FC = () => {
   const navigate = useNavigate();
   const [removeBreed] = useRemoveBreedMutation();
+  const [dataBreed, setDataBreed] = useState<any | null>(null);
+  const { data } = useGetAllBreedQuery();
+  useEffect(() => {
+    if (data) {
+      setDataBreed(data);
+    }
+  }, [data]);
+
+  const [filter, setFilter] = useState({ name: ""});
+  const [openReset, setOpenReset] = useState<boolean>(false);
+
+  const handleFilterChange = (fieldName: string, value: string) => {
+    setFilter({ ...filter, [fieldName]: value });
+  };
   const confirm = (id: number) => {
     removeBreed(id)
       .then((response: any) => {
@@ -43,9 +58,9 @@ const BreedAdmin: React.FC = () => {
       width: 150,
     },
     {
-      title: "Giống",
-      dataIndex: "nameSpecies",
-      key: "nameSpecies",
+      title: "Loài",
+      dataIndex: "speciesName",
+      key: "speciesName",
       width: 150,
     },
     {
@@ -78,9 +93,43 @@ const BreedAdmin: React.FC = () => {
     },
   ];
 
-  const { data } = useBreedQuery(1);
+  useEffect(() => {
+    const filteredData = data?.filter((item) =>
+      item.name?.toLowerCase().includes(filter.name.trim().toLowerCase())
+    );
+    setDataBreed(filteredData);
+  }, [data, filter]);
+  
+
+  useEffect(() => {
+    if (filter.name === "") {
+      setOpenReset(false);
+    } else {
+      setOpenReset(true);
+    }
+  }, [filter.name]);
   return (
     <>
+    <div
+        className="btn-table"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <div style={{ display: "flex", columnGap: 20 }}>
+          <Search
+            placeholder="Tìm tên giống"
+            value={filter?.name}
+            onChange={(e) => handleFilterChange("name", e.target.value)}
+            style={{ width: 200, marginBottom: 10 }}
+          />
+          <Button
+            onClick={() => setFilter({ name: ""})}
+            danger
+            disabled={!openReset}
+          >
+            Cài lại
+          </Button>
+        </div>
+      </div>
       <Button
         type="primary"
         onClick={() => navigate("add")}
@@ -89,7 +138,7 @@ const BreedAdmin: React.FC = () => {
       >
         THÊM GIỐNG
       </Button>
-      <TableAdmin columns={columns} data={data} />
+      <TableAdmin columns={columns} data={dataBreed} />
     </>
   );
 };
