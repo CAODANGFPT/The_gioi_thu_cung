616185eb7c28f88a2e3139e7519f15a791332044
Thanh03Dev <trongthanh2003.tn@gmail.com>
2023-12-14 14:06:12 +0700

    feat: get api footer table client

--- front-end/src/layout/base/footer.tsx
+++ front-end/src/layout/base/footer.tsx
@@ -1,82 +1,59 @@
 import "../../assets/scss/layout/base/footerBase.scss";
-import logo from "../../assets/image/logo.png";
 import appStore from "../../assets/image/appStore.png";
 import FacebookIcon from "../../assets/svg/facebookIcon";
 import InstagramIcon from "../../assets/svg/instagramIcon";
 import TwitterIcon from "../../assets/svg/twitterIcon";
 import YoutubeIcon from "../../assets/svg/youtubeIcon";
+import { useGetAllFooterQuery } from "../../services/footer";
+import { useGetAllWebsiteInformationQuery } from "../../services/websiteInformation";
 
-const FooterBase = () => {
+const FooterBase: React.FC = () => {
+  const { data: listFooter } = useGetAllFooterQuery();
+  const { data: listWebsiteInformation } = useGetAllWebsiteInformationQuery();
+
+  const footerContentLeft =
+    listFooter && listFooter.length > 0 ? listFooter[0].content_left : "";
+
+  const footerContentRight =
+    listFooter && listFooter.length > 0 ? listFooter[0].content_right : "";
+
   return (
     <>
       <div className="containerFooter">
         <div className="containerFooter-1">
           <div className="logo">
-            <img src={logo} alt="logo" />
-            <p className="logo-title">Chúng tôi có mọi thứ cho mèo ở đây!</p>
+            <img
+              src={
+                listWebsiteInformation &&
+                listWebsiteInformation.length > 0 &&
+                typeof listWebsiteInformation[0]?.logo === "string"
+                  ? listWebsiteInformation[0]?.logo
+                  : undefined
+              }
+              alt="Ảnh logo"
+            />
+            <p className="logo-title">
+              {listFooter && listFooter.length > 0 && listFooter[0].slogan}
+            </p>
           </div>
           <div className="group18">
-            <p className="group18-title-1">Theo dõi bản tin của chúng tôi</p>
-            <p className="group18-title-2">Blog mới về mèo mỗi tuần</p>
-            <div className="group18-input">
-              <input type="text" placeholder="ĐỊA CHỈ EMAIL CỦA BẠN" />
-              <button>GỬI EMAIL</button>
-            </div>
+            {footerContentLeft && (
+              <div
+                className="group18"
+                dangerouslySetInnerHTML={{ __html: footerContentLeft }}
+              />
+            )}
           </div>
         </div>
-        <div className="containerFooter-2">
-          <div className="group8">
-            <p className="group8-title-1">Thứ tự đặt lịch của tôi đâu?</p>
-            <button>THEO DÕI THỨ TỰ</button>
-            <p className="group8-title-2">
-              Xin lưu ý, đặt lịch chăm sóc thú cưng PetCare chỉ nhận lịch trước
-              5 ngày để đảm bảo tính uy tín của cửa hàng và lịch của khách.
-            </p>
-          </div>
-          <div className="group8">
-            <p className="group8-title-1">Đang chuyển hàng</p>
-            <p className="group8-title-2">Về Miễn Phí Vận Chuyển</p>
-            <p className="group8-title-2">Thông tin vận chuyển</p>
-          </div>
-          <div className="group8">
-            <p className="group8-title-1">Ứng dụng mua sắm</p>
-            <p className="group8-title-2">
-              Hãy dùng thử tính năng Xem trong phòng của bạn, quản lý đăng ký và
-              lưu thông tin thanh toán.
-            </p>
-            <img src={appStore} alt="" />
-          </div>
-          <div className="group8">
-            <p className="group8-title-1">Công ty chúng tôi</p>
-            <p className="group8-title-2">Về chúng tôi</p>
-            <p className="group8-title-2">Nghề nghiệp</p>
-            <p className="group8-title-2">Liên hệ</p>
-            <p className="group8-title-2">Địa điểm cửa hàng</p>
-          </div>
-          <div className="group8">
-            <p className="group8-title-1">Truyền thông xã hội</p>
-            <div className="icon">
-              <FacebookIcon />
-              <InstagramIcon />
-              <YoutubeIcon />
-              <TwitterIcon />
-            </div>
-            <p className="group8-title-2">
-              Cho chúng tôi xem con mèo của bạn với hastag: #meowshop
-              #themeowshop #petcare #pet_care
-            </p>
-          </div>
-          <div className="group8">
-            <p className="group8-title-1">Chính sách</p>
-            <p className="group8-title-2">Chính sách vận chuyển</p>
-            <p className="group8-title-2">Chính sách hoàn tiền</p>
-            <p className="group8-title-2">Chính sách bảo mật</p>
-            <p className="group8-title-2">Điều khoản dịch vụ</p>
-          </div>
-        </div>
+        {footerContentRight && (
+          <div
+            className="containerFooter-2"
+            dangerouslySetInnerHTML={{ __html: footerContentRight }}
+          />
+        )}
       </div>
       <div className="copyright">
-        <p>Copyright © 2022 The Pet Care Shop. Đã đăng ký Bản quyền</p>
+        <p>{listFooter && listFooter.length > 0 && listFooter[0].license}</p>
       </div>
     </>
   );
